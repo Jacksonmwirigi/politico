fa2d521d2483cece69c913a7e79b57a898fdd56f
Jackson Ithalii <jacksonmwirigi@gmail.com>
2019-02-01 17:33:10 +0300

    updated files

---
+++ ui/js/voting.js
@@ -0,0 +1,10 @@
+
+var open_ab=0;
+displayTabs(open_tab);
+function displayTabs (n){
+     x =document.getElementsByClassName("tabview")
+     x[n].style.display = "block";
+     alert("testing");
+    
+    
+}
