74897026277a9db1f2cbbc6ac2f309728992b9db
Jackson Ithalii <jacksonmwirigi@gmail.com>
2019-02-01 23:57:36 +0300

    [feature #163533647] improvements

---
+++ ui/js/voting.js
@@ -0,0 +1,16 @@
+
+var open_ab=0;
+displayTabs(open_tab);
+function displayTabs (n){
+     x =document.getElementsByClassName("tabview")
+     x[n].style.display = "block";
+     
+    if(n==0){
+
+    }
+    else{
+        
+    }
+
+    
+}
