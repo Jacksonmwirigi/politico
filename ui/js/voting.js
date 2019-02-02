f45821afc96e1fc8330e1f02c7a102ce91a66bb8
Jackson Ithalii <jacksonmwirigi@gmail.com>
2019-02-02 21:43:00 +0300

    Added login page authentication by javascript

--- ui/js/voting.js
+++ ui/js/voting.js
@@ -1,16 +1,70 @@
+var currentTab = 0;
+showTab(currentTab);
 
-var open_ab=0;
-displayTabs(open_tab);
-function displayTabs (n){
-     x =document.getElementsByClassName("tabview")
-     x[n].style.display = "block";
-     
-    if(n==0){
-
+function showTab(n) {
+    // This function will display the specified tab of the form...
+    var x = document.getElementsByClassName("tabview");
+    x[n].style.display = "block";
+    //... and fix the Previous/Next buttons:
+    if (n == 0) {
+        document.getElementById("prevBtn").style.display = "none";
+    } else {
+        document.getElementById("prevBtn").style.display = "inline";
     }
-    else{
-        
+    if (n == (x.length - 1)) {
+        document.getElementById("nextBtn").innerHTML = "Submit";
+    } else {
+        document.getElementById("nextBtn").innerHTML = "Next";
+    }
+    //below function calculates the number of steps depending on the size of your tabs 
+    fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+    // This function will figure out which tab to display
+    var x = document.getElementsByClassName("tabview");
+    if (n == 1 && !validateForm()) return false;
+    // Hide the current tab:
+    x[currentTab].style.display = "none";
+
+    currentTab = currentTab + n;
+
+    if (currentTab >= x.length) {
+        // if you have reached the end of the form, the form gets submitted:
+        document.getElementById("tab_form").submit();
+        return false;
     }
 
-    
+    showTab(currentTab);
+}
+
+function validateForm() {
+
+    var x, y, i, valid = true;
+    x = document.getElementsByClassName("tabview");
+    y = x[currentTab].getElementsByTagName("input");
+
+    for (i = 0; i < y.length; i++) {
+
+        if (y[i].value == "") {
+
+            y[i].className += " invalid";
+
+            valid = false;
+        }
+    }
+
+    if (valid) {
+        document.getElementsByClassName("steps")[currentTab].className += " finish";
+    }
+    return valid;
+}
+function fixStepIndicator(n) {
+
+    var i, x = document.getElementsByClassName("steps");
+    for (i = 0; i < x.length; i++) {
+        x[i].className = x[i].className.replace("active", "");
+    }
+
+    x[n].className += " active";
 }
