74f4d092c5017e06a082225d663c9acff042dc90
jacksonmwirigi@gmail.com <jacksonmwirigi@gmail.com>
2019-02-19 21:17:23 +0300

    added login end point

--- app/api/v2/models/user_model.py
+++ app/api/v2/models/user_model.py
@@ -1,52 +1,77 @@
-
+from app.api.v2.db.db_config import init_db
+import psycopg2
 from app.api.v2.models.base_model import BaseModel
-from app.api.v2.db.db_config import* 
-import jwt
-from config import Config 
-import datetime
 
-class UserModel(BaseModel):
-    def __init__(self,first_name="first_name", second_name="second_name",
-     other_name="other_name", email="email", passportUrl="passport",
-     phone_number="phone_number", isadmin="isadmin", iscandidate="iscandidate"):
-        """creating user instance """
+con = init_db()
+cur = con.cursor()
+class UserModel():
+    """creating a user object."""
+    def __init__(self,first_name, second_name,other_name,passport_url, 
+    email_address ,phone_number, is_admin):
+        """creating user class instance variables """
         self.first_name =first_name
         self.second_name =second_name
         self.other_name =other_name
-        self.email =email
+        self.passport_url=passport_url
+        self.email_address = email_address
         self.phone_number =phone_number
-        self.passportUrl=passportUrl
-        self.isadmin =isadmin
-        self.iscandidate =iscandidate
+        self.is_admin =is_admin
+        
+       
 
-    def register_new(self,first_name, second_name, other_name, 
-    email,passportUrl ,phone_number,isadmin, iscandidate):
-        """This function registers a new user in the system."""
-        # user = {
-        #     "first_name": first_name,
-        #     "second_name": second_name,
-        #     "other_name": other_name,
-        #     "email": email,
-        #     "phone_number": phone_number,
-        #     "passportUrl": passportUrl,
-        #     "isAdmin": isAdmin,
-        #     "isCandidate":isCandidate
-        # }
-
+    def register_user(self):
+        """registers  a new user in the database -politicodb"""
         con = init_db()
         cur = con.cursor()
-
-        if BaseModel().check_if_exists('users', 'passportUrl', passportUrl) ==True :
-            """Checks for existing record of a user before registering a new account"""
-            return "user is already registered"
-
-        query = """ INSERT INTO users (first_name, second_name, 
-        other_name, email,phone_number,passportUrl, isadmin,iscandidate) VALUES \
-            ( '{}', '{}', '{}', '{}', '{}', '{}','{}','{}') RETURNING user_id """.format(first_name, second_name, 
-        other_name, email,phone_number,passportUrl, isadmin,iscandidate)
-        cur.execute(query)
-        user_id = cur.fetchone()[0]
+ 
+        query = """ INSERT INTO users (first_name, second_name,other_name,passport_url,
+        email_address,phone_number, is_admin) 
+        VALUES(%s,%s, %s,%s , %s, %s, %s)"""
+         
+        new_entry = (self.first_name,self.second_name, self.other_name, 
+        self.passport_url, self.email_address, self.phone_number, self.is_admin)
+                       
+        cur.execute(query, new_entry)
+        
         con.commit()
         cur.close()
-        return user_id
+        return new_entry
 
+    @classmethod  
+    def get_all_users(cls):
+        """defines method for viewing all registered users."""
+        con = init_db()
+        cur = con.cursor()
+        get_query= """ SELECT * FROM users""" 
+        cur.execute(get_query)
+        users= cur.fetchall()
+        return users
+
+    @classmethod
+    def user_login(cls, phone_number):
+        """Defines user login method"""
+        login_query=""" SELECT phone_number FROM users WHERE phone_number=%s"""
+        cur.execute(login_query) 
+        loged_user= cur.fetchone(phone_number)
+        return loged_user
+
+    @classmethod
+    def get_email_address(cls, email_address):
+        """Gets specific user's email address to allow login"""
+        cur.execute("""SELECT * FROM users WHERE email_address='%s'"""%(email_address))  
+        user =cur.fetchone()
+        con.commit()
+        # cur.close()
+        return user
+
+
+       
+
+
+        
+
+
+
+
+
+    
---
+++ app/api/v2/views/auth/user_view.py
@@ -0,0 +1,82 @@
+from app.api.v2.models.user_model import UserModel
+from app.api.v2.models.base_model import BaseModel
+from flask import Flask,Blueprint ,make_response, jsonify, request
+from flask.views import MethodView
+from app.validation import*
+
+register_bluprint =Blueprint("sign_up", __name__,url_prefix='/api/v2')
+login_bluprint=Blueprint("login",__name__, url_prefix= '/api/v2')
+
+@register_bluprint.route('/signup', methods=['POST'])
+def register():
+    """Registers new user entry into the database. """   
+    key_error= is_register_key_correct(request)
+    if key_error:
+        return make_response(jsonify({
+            "Error" :400,
+            "msg" : "Invalid keys "
+        })) 
+
+    data= request.get_json()
+    first_name=data['first_name']
+    second_name=data['second_name']
+    other_name=data['other_name']
+    passport_url=data['passport_url']
+    email_address=data['email_address']
+    phone_number=data['phone_number']
+    is_admin=data['is_admin']
+     
+    new_user=UserModel(first_name,second_name,other_name,
+         passport_url,email_address,phone_number,is_admin).register_user()
+    if new_user:
+        token =BaseModel().encode_auth_token( email_address)
+    else :
+        return make_response(jsonify({
+            "Error" :404,
+            "msg" : "No data provided"
+    }))   
+
+
+    return make_response(jsonify({
+        'status': 201,
+        'message': 'Successfully registered.',
+        'data': new_user,
+        'token' : token.decode()
+    }),201)
+
+
+@register_bluprint.route('/signup', methods=['GET'])
+def view_users():
+    """retrieves all the registered users in the db. only requires admin previleges"""
+    users= UserModel.get_all_users()
+    if users:
+        return make_response(jsonify({
+            'status': 200,
+            'message': 'Successful',
+            'data': users
+        }),200)
+    return make_response(jsonify({
+        'status': 404,
+        'message': 'Sorry No registred user yet'
+
+    }),404)    
+
+@login_bluprint.route('/login', methods= ['POST'])
+def user_signIn():
+    """This end point allows regietered users to log in the system"""
+    data= request.get_json()
+    email_address =data['email_address']
+
+    user= UserModel.get_email_address(email_address)
+    if user:
+        token =BaseModel().encode_auth_token(email_address)
+        return make_response(jsonify({
+            "status": 200,
+            "msg": "signed In successfully",
+            "data" : user,
+            "token": token.decode()
+        }),200)
+    return make_response(jsonify({
+        "error" :404,
+        "msg" : "This email provided is not registered"
+    }),404)   
